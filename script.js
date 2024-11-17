6ff014f2450ff76c2a1125bd6b387d92df8ccd54
Akshat Saxena <akshatsaxena890@gmail.com>
2024-11-18 00:19:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript to play the birthday song after page load
+window.onload = function() {
+    var audio = document.getElementById("birthdaySong");
+    audio.play(); // Play the birthday song automatically when the page loads
+};
